631b90dabeddd94c5802b15e3b1a4deb53b08b7a
lakhwinder <lakhwindersingh@yahoo.com>
2022-07-30 01:18:03 -0400

    new data feeds

--- tensortrade/data/simdd.py
+++ tensortrade/data/simdd.py
@@ -4,22 +4,33 @@ https://www.cryptodatadownload.com.
 
 import ssl
 
+import numpy as np
 import pandas as pd
-
 import yfinance as yf
+
+import requests 
+import warnings
+import gzip
+import os
+import math
+
+from datetime import datetime
+from dateutil import rrule
+from datetime import datetime
+import tensortrade.stochastic as sp 
 
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-class YahooDataDownload:
+class SimulatedDataDownload:
     """Provides methods for retrieving data on different cryptocurrencies from
     https://www.cryptodatadownload.com/cdd/.
 
     Attributes
     ----------
     url : str
-        The url for collecting data from CryptoDataDownload.
+        The url for collecting data from YahooDataDownload.
 
     Methods
     -------
@@ -29,10 +40,8 @@ class YahooDataDownload:
     """
 
     def __init__(self) -> None:
-
         self.url = "https://www.cryptodatadownload.com/cdd/"
 
- 
     def fetch_default(self,
                       exchange_name: str,
                       base_symbol: str,
@@ -60,42 +69,15 @@ class YahooDataDownload:
             A open, high, low, close and volume for the specified exchange and
             cryptocurrency pair.
         """
-        warnings.filterwarnings("ignore")
-        pd.options.display.float_format = '{:.4%}'.format
+        data = sp.cox(
+            base_price=1000,
+            base_volume=5,
+            start_date="2010-01-01",
+            times_to_generate=1000,
+            time_frame=timeframe
+        )
+        return data
 
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-        
-        filename = "{}_{}{}_{}.csv".format(exchange_name, quote_symbol, base_symbol, timeframe)
-        base_vc = "Volume {}".format(base_symbol)
-        new_base_vc = "volume_base"
-        quote_vc = "Volume {}".format(quote_symbol)
-        new_quote_vc = "volume_quote"
-       
-        start = '2016-01-01'
-        end = datetime.today().strftime('%Y-%m-%d')
-
-        df = yf.download(x, start = start, end = end)
-        #df['asset'] = x
-        df.ffill(axis = 0)
-        
-        #df = pd.read_csv(self.url + filename, skiprows=1)
-        #df = df[::-1]
-        #df = df.drop(["symbol"], axis=1)
-        df = df.rename({base_vc: new_base_vc, quote_vc: new_quote_vc, "Date": "date"}, axis=1)
-
-        if "d" in timeframe:
-            df["date"] = pd.to_datetime(df["date"])
-        elif "h" in timeframe:
-            df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d %H:%M:%S")
-
-        df = df.set_index("date")
-        df.columns = [name.lower() for name in df.columns]
-        df = df.reset_index()
-        if not include_all_volumes:
-            df = df.drop([new_quote_vc], axis=1)
-            df = df.rename({new_base_vc: "volume"}, axis=1)
-            return df
-        return df
 
    
     def fetch(self,
@@ -125,6 +107,7 @@ class YahooDataDownload:
             A open, high, low, close and volume for the specified exchange and
             cryptocurrency pair.
         """
+
         return self.fetch_default(exchange_name,
                                   base_symbol,
                                   quote_symbol,
